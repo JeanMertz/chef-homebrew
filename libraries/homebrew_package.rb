139fe434388b68785942e6b615cf1391c86d222e
Christopher Brown <cb@opscode.com>
2011-10-02 09:05:36 -0700

    added 'brew upgrade' and changed 'run_command' to 'get_response_from_command' to control return value

--- libraries/homebrew_package.rb
+++ libraries/homebrew_package.rb
@@ -34,10 +34,8 @@ class Chef
           end
         end
 
-        # Homebrew doesn't really have a notion of upgrading packages, just
-        # install the latest version?
         def upgrade_package(name, version)
-          install_package(name, version)
+          brew('upgrade', name)
         end
 
         def remove_package(name, version)
@@ -52,9 +50,7 @@ class Chef
         protected
 
         def brew(*args)
-          run_command_with_systems_locale(
-            :command => "brew #{args.join(' ')}"
-          )
+          get_response_from_command("brew #{args.join(' ')}")
         end
 
         def checkout_formula_for name, version
